438f3010fd14cf5aaeeb327f32eeaffdfad12cdf
aicsoft <aicsoft@yandex.ru>
2022-08-20 10:36:07 +0500

    Second commit

---
+++ src/main/java/com/example/javamy/Core/level13/_31_Repka.java
@@ -0,0 +1,30 @@
+package com.example.javamy.Core.level13;
+
+public class _31_Repka {
+
+    public class Person {
+        private String name;
+        private String namePadezh;
+
+        public Person(String name, String namePadezh) {
+            this.name = name;
+            this.namePadezh = namePadezh;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getNamePadezh() {
+            return namePadezh;
+        }
+
+        public void setNamePadezh(String namePadezh) {
+            this.namePadezh = namePadezh;
+        }
+    }
+}
--- src/main/java/com/example/javamy/Core/level13/level13_lekciya11_nom30_Sort_chet_chisel_from_file.java
+++ src/main/java/com/example/javamy/Core/level13/level13_lekciya11_nom30_Sort_chet_chisel_from_file.java
@@ -1,36 +1,101 @@
 package com.example.javamy.Core.level13;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class level13_lekciya11_nom30_Sort_chet_chisel_from_file {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String read = reader.readLine();
-        Scanner scanner = new Scanner(read);
-        ArrayList list = new ArrayList();
 
-        if (scanner.hasNextLine()) {
-           list.add(Integer.parseInt(scanner.nextLine()));
+        FileInputStream fileInputStream = new FileInputStream(read);
+        Scanner scanner = new Scanner(fileInputStream);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while(scanner.hasNextLine()) {
+            int dig = Integer.parseInt(scanner.nextLine());
+            if (dig%2 ==0) {
+                list.add(dig);
+            }
         }
 
-        System.out.println(list.toString());
+        Collections.sort(list);
+        for(Integer d:list){
+            System.out.println(d);
+        }
 
-//        for (int i = 0; i <list.size() ; i++) {
-//            for (int j = 0; j < list.size(); j++) {
-//                if (list[i]>list.get(j)){
-//                    int d = list.get(j);
-//                    list.get(i) = list.get(j);
-//                    list.get(j) = d;
-//
-//                }
-//            }
-//        }
-
-
+        fileInputStream.close();
+        scanner.close();
+        reader.close();
     }
 }
+
+
+
+
+
+
+
+/*
+
+import java.io.BufferedReader;
+        import java.io.FileInputStream;
+        import java.io.InputStreamReader;
+        import java.util.ArrayList;
+        import java.util.Collections;
+        import java.util.List;
+        import java.util.Scanner;
+
+*/
+/*
+Сортировка четных чисел из файла
+*//*
+
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String sourceFileName = reader.readLine();
+        Scanner scanner = new Scanner(new FileInputStream(sourceFileName));
+
+        List<Integer> data = new ArrayList<>();
+        while (scanner.hasNext()) {
+            int value = scanner.nextInt();
+            if (value % 2 == 0) data.add(value);
+        }
+
+        Collections.sort(data);
+        for (Integer value : data) {
+            System.out.println(value);
+        }
+
+        scanner.close();
+    }
+}
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
