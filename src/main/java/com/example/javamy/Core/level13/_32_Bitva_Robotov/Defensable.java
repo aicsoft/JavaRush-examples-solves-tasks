9d9f832baa0431acb2a8c079a487d7f2c6354dd7
aicsoft <aicsoft@yandex.ru>
2022-08-21 07:10:57 +0500

    3rd commit

---
+++ src/main/java/com/example/javamy/Core/level13/_32_Bitva_Robotov/Defensable.java
@@ -0,0 +1,6 @@
+package com.example.javamy.Core.level13._32_Bitva_Robotov;
+
+
+public interface Defensable {
+    BodyPart defense();
+}
---
+++ src/main/java/com/example/javamy/Core/level13/_32_Bitva_Robotov/Robot.java
@@ -0,0 +1,23 @@
+package com.example.javamy.Core.level13._32_Bitva_Robotov;
+
+public class Robot extends AbstractRobot implements Attackable, Defensable {
+
+    private String name;
+
+    public Robot(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+
+}
+
+
+
+
+
+
